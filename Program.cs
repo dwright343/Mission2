4f580359fd2d97796568c3b56d1e65f25de063c1
dwright6633 <103792319+dwright6633@users.noreply.github.com>
2024-01-18 07:13:55 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Danny Wright
+// Section 4
+/* Write a .NET console application using C# that simulates the rolling of two 6-sided dice. Use an Array 
+to keep track of the number of times that each combination is thrown. In other words,
+keep track of how many times the combination of the two simulated dice is 2, how many times
+the combination is 3, and so on, all the way up through 12.
+
+Allow the user to choose how many times the “dice” will be thrown. Then, once you have the
+number of rolls, pass that number to a second class that has a method that simulates the roll of
+the dice for the number of times that the user specified. That method in the second class should
+return the array containing the results. In the first class, use that array to print a histogram (using
+the* character) that shows the total percentage each number was rolled. Each * will represent
+1% of the total rolls.*/
+using Mission2;
+
+RollDice rd = new RollDice();
+
+Console.WriteLine("Welcome to the dice throwing simulator!\r\n" +
+    "How many dice rolls would you like to simulate?");
+int iNumRolls = int.Parse(System.Console.ReadLine());
+
+Console.WriteLine($"DICE ROLLING SIMULATION RESULTS\nEach '*' represents 1% of the total number of rolls.\n" +
+                  $"Total number of rolls = {iNumRolls}.");
+
+rd.Roll(iNumRolls);
+
+Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+
+Console.ReadLine();
---
+++ RollDice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    internal class RollDice
+    {
+        public void Roll(int iRolls)
+        {
+            // Create a array for possible roll numbers.
+            int[] rollTotals = new int[13];
+
+            for (int iCount = 0; iCount < iRolls; iCount++)
+            {
+                Random rnd = new Random();
+                int iDieRoll1 = rnd.Next(1, 7);
+
+                Random rnd2 = new Random();
+                int iDieRoll2 = rnd.Next(1, 7);
+
+                int iRollCombo = iDieRoll1 + iDieRoll2;
+
+                // rollTotals[total]++;
+                rollTotals[iRollCombo] = (rollTotals[iRollCombo] + 1);
+            }
+
+            // Calculate the percentage of times that the number was rolled
+            for (int iCount2 = 2; iCount2 < rollTotals.Length; iCount2++)
+            {
+                // percentage = how many times the specific number was rolled / total number of rolls * 100
+                string asterisks = new string('*', (rollTotals[iCount2]  * 100 / iRolls));
+
+                Console.WriteLine($"{iCount2}: {asterisks}");
+            }        
+        }
+    }
+}
